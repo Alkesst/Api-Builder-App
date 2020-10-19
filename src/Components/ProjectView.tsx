7bfa7d07c3e3ac12430efffb19b76900d29f8dc7
Alejandro Garau Madrigal <alegarmad@uma.es>
2020-10-19 20:40:56 +0200

    Installed the font awesom icon packs and improved the ProjectView component

--- src/Components/ProjectView.tsx
+++ src/Components/ProjectView.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Project } from 'api-builder-types';
+import Button from 'react-bootstrap/cjs/Button';
+import Collapse from 'react-bootstrap/cjs/Collapse';
+import Container from 'react-bootstrap/cjs/Container';
+import Row from 'react-bootstrap/cjs/Row';
+import Col from 'react-bootstrap/cjs/Col';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 interface ProjectViewProps extends Project {}
 
@@ -7,23 +14,41 @@ const ProjectView: React.FC<ProjectViewProps> = ({
     Name,
     Description,
     Type,
-}: ProjectViewProps) => (
-    <>
-        <div>
-            <div>
-                Name:
-                {Name}
-            </div>
-            <div>
-                Type:
-                {Type}
-            </div>
-            <div>
-                Description:
-                {Description}
-            </div>
-        </div>
-    </>
-);
+}: ProjectViewProps) => {
+    const [open, setOpen] = useState<boolean>(false);
+
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Col sm={8}>
+                        {Name}
+                    </Col>
+                    <Col sm={4}>
+                        <Button
+                            onClick={() => setOpen(!open)}
+                            aria-controls="example-collapse-text"
+                            aria-expanded={open}
+                        >
+                            <FontAwesomeIcon icon={faAngleDown} />
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+            <Collapse in={open}>
+                <div>
+                    <div id="example-collapse-text">
+                        Type:
+                        {` ${Type}`}
+                    </div>
+                    <div>
+                        Description:
+                        {` ${Description}`}
+                    </div>
+                </div>
+            </Collapse>
+        </>
+    );
+};
 
 export default ProjectView;
