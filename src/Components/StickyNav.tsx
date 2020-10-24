ed3f0b845dcec98e860c2686ac0da7a7325b6124
Alejandro Garau Madrigal <alegarmad@uma.es>
2020-10-24 17:49:54 +0200

    Added navbar and related work

--- src/Components/StickyNav.tsx
+++ src/Components/StickyNav.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/cjs/Navbar';
+import Nav from 'react-bootstrap/cjs/Nav';
 
-const Navbar = () => {
-    const jose = 1;
+const StickyNav = () => {
+    const jose = 'Features';
+
+    const generateFront = () => {
+        const hasSession = sessionStorage.getItem('userToken');
+        return (hasSession) ? (<Nav.Link href="#features">{jose}</Nav.Link>) : (<Nav.Link href="/login"> Login </Nav.Link>);
+    };
+
     return (
         <>
-            {jose}
+            <Navbar bg="dark" sticky="top" expand="lg" className="navbar-dark">
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Brand href="#home">
+                    <img
+                        src="/esplota.png"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        {generateFront()}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </>
     );
 };
 
-export default Navbar;
+export default StickyNav;
