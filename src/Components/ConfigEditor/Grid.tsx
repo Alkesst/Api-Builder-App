c344304608cca3b83a51f9079e8941238f9cf252
Alkesst <alec.augal@gmail.com>
2021-04-18 12:57:53 +0200

    Fixed errors

--- src/Components/ConfigEditor/Grid.tsx
+++ src/Components/ConfigEditor/Grid.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { IEntity } from 'api-builder-types';
 import { IRelationship } from 'api-builder-types/relationship';
 import Entity from './MinorComponents/Entity';
@@ -12,11 +12,9 @@ interface IGridProps {
 }
 
 const Grid : React.FC<IGridProps> = ({ expanded, projectEntities, loaded }: IGridProps) => {
-    const [entityReferences, setEntityReferences] = useState<EntityReference[]>([]);
-
-    const generateReferences = useCallback(() => {
+    const references: EntityReference[] = useMemo(() => {
+        const tempRefObject: EntityReference[] = [];
         if (projectEntities && projectEntities.length > 0) {
-            const tempRefObject: EntityReference[] = [];
             projectEntities.forEach((element: IEntity) => {
                 if (hasRelationships(element)) {
                     element.Relationships.forEach((relation: IRelationship) => {
@@ -27,29 +25,26 @@ const Grid : React.FC<IGridProps> = ({ expanded, projectEntities, loaded }: IGri
                     });
                 }
             });
-            setEntityReferences(tempRefObject);
         }
-    }, []);
+        return tempRefObject;
+    }, [projectEntities]);
 
-    const generateEntities = useMemo(() => {
-        generateReferences();
-        return projectEntities.map((entity: IEntity) => {
-            const entityRef = getEntityReference(entityReferences, entity.Identifier.toString());
-            return (
-                <div ref={entityRef} key={`div-${entity.Identifier.toString()}`}>
-                    <Entity
-                        key={entity.Identifier.toString()}
-                        Identifier={entity.Identifier}
-                        Name={entity.Name}
-                        Relationships={entity.Relationships}
-                        Attributes={entity.Attributes}
-                        Coordinates={entity.Coordinates}
-                        Constraints={entity.Constraints}
-                    />
-                </div>
-            );
-        });
-    }, [entityReferences, projectEntities, generateReferences]);
+    const generateEntities = useMemo(() => projectEntities.map((entity: IEntity) => {
+        const entityRef = getEntityReference(references, entity.Identifier.toString());
+        return (
+            <div ref={entityRef} key={`div-${entity.Identifier.toString()}`}>
+                <Entity
+                    key={entity.Identifier.toString()}
+                    Identifier={entity.Identifier}
+                    Name={entity.Name}
+                    Relationships={entity.Relationships}
+                    Attributes={entity.Attributes}
+                    Coordinates={entity.Coordinates}
+                    Constraints={entity.Constraints}
+                />
+            </div>
+        );
+    }), [projectEntities, references]);
 
     /*
     * const recalculateRelationship = () => setRelationships(Relationships.map((item) => (
