3b0930ca367eb58a85857dbcf323c833801d5ee9
Alejandro Garau Madrigal <alegarmad@uma.es>
2021-06-19 11:51:55 +0200

    Updated styles for the entities and attributes

--- src/Components/ConfigEditor/MinorComponents/Attribute.tsx
+++ src/Components/ConfigEditor/MinorComponents/Attribute.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { AttributeType, IAttribute } from 'api-builder-types/attribute';
+import '../../../Styles/ConfigEditor/Attributes.scss';
 
 interface IAttributeProps extends IAttribute {
 }
 
 const Attribute : React.FC<IAttributeProps> = ({ Name, Type }: IAttributeProps) => (
-    <div className="flex content-space-between padding-sides-5 padding-top-5">
+    <div className="flex content-space-between padding-sides-5 padding-top-5 Attribute">
         <div>
             {Name}
         </div>
