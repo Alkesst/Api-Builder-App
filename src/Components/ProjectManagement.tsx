e69fc8103c53b00e5a2a9bee955b038ead641ade
Alkesst <alec.garau@gmail.com>
2021-08-17 10:46:08 +0200

    Filtering projects

--- src/Components/ProjectManagement.tsx
+++ src/Components/ProjectManagement.tsx
@@ -1,28 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { IProject } from 'api-builder-types';
 import { retrieveProjects } from 'Helper/Retriever';
 import { ProjectView } from 'Components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const ProjectsView : React.FC = () => {
     const [projects, setProjects] = useState<IProject[]>();
     const [fetching, setFetching] = useState<boolean>(false);
+    const [filteredProjects, setFilteredProjects] = useState<IProject[]>();
 
     useEffect(() => {
         if (!projects) {
             setFetching(true);
             retrieveProjects().then((result: IProject[]) => {
                 setProjects(result);
+                setFilteredProjects(result);
                 setFetching(false);
             });
         }
     }, [projects]);
 
+
+    const handleFiltering = (event: ChangeEvent<HTMLInputElement>) => {
+        if(event.target.value === '' || !event.target.value) {
+            setFilteredProjects(projects); 
+            return;
+        }
+        const a = filteredProjects?.filter((e: IProject) => e.Name.includes(event.target.value) || e.Type.toString().includes(event.target.value));
+        setFilteredProjects(a);
+    }
+
     return (
         <div className="App App-Background text-gainsboro projects-container-align">
-            <h1 className="padding-top-15 padding-left-15">Projects</h1>
+            <div className="flex justify-content-between align-items-center padding-right-15">
+                <h1 className="padding-top-15 padding-left-15">Projects</h1>
+                <div>
+                    <FontAwesomeIcon icon={faSearch} title="Mandatory"/>
+                    <input className="margin-left-15" onChange={(e) => handleFiltering(e)} />
+                </div>
+            </div>
             <div className="App-header App-Background-Height-Title Center-Content padding-top-15">
                 {fetching && <div>Loading...</div>}
-                {projects && projects.map((project) => (
+                {filteredProjects && filteredProjects.map((project) => (
                     <ProjectView
                         key={project.Identifier.toString()}
                         Identifier={project.Identifier}
