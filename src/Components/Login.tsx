84fa15376dc7866d95c7e5e8e1ddf44c0e797cf1
Aleks <alec.augal@gmail.com>
2021-09-19 15:55:43 +0200

    Ahora con un 100% menos de faltosidad (#30)
    Co-authored-by: Alkesst <alec.garau@gmail.com>

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -3,8 +3,8 @@ import { login } from 'Helper/Retriever';
 
 const Login : React.FC = () => (
     <>
-        esto es un puto login,
-        <button type="button" onClick={login}>Click aqui anda puta zorra</button>
+        Esto es un login,
+        <button type="button" onClick={login}>Click para login</button>
     </>
 );
 
